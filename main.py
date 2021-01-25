65e2e705158032f92f9e5e0e528bcf3d62434a50
cheubes <christophe@heubes.com>
2021-01-25 09:53:58 +0100

    Pass test_different_period.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from time_calculator import add_time
 from unittest import main
 
 
-
+# add_time("3:30 PM", "2:12")
+# add_time("11:55 AM", "3:12")
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
--- time_calculator.py
+++ time_calculator.py
@@ -1,23 +1,29 @@
 
 class time :
+    periods = {0:'AM', 1:'PM'}
+    periods_idx = {'AM':0, 'PM':1}
+
     hours = 0
     minutes = 0
-    period = 'AM'
+    period = 0
 
     def __init__(self, time_as_string):
         tas = time_as_string.split()
-        self.period = tas[1]
+        self.period = self.periods_idx[tas[1]]
         t = tas[0].split(':')
         self.hours = int(t[0])
         self.minutes = int(t[1])
 
     def add(self, duration):
         d = duration.split(':')
-        self.hours += int(d[0])
-        self.minutes += int(d[1])
+        m = divmod(self.minutes + int(d[1]), 60)
+        self.minutes = m[1]
+        h = divmod(self.hours + int(d[0]) + m[0], 12)
+        self.hours = h[1]
+        self.period = (self.period + h[0]) % 2
 
     def __str__(self):
-        return f'{self.hours}:{self.minutes} {self.period}'
+        return f'{self.hours}:{self.minutes:02d} {self.periods[self.period]}'
 
 def add_time(start, duration, day = None):
 
