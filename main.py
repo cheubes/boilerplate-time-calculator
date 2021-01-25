f0150ba4f39c1a5b2cc73f35e2dbcb7316de4ff3
cheubes <christophe@heubes.com>
2021-01-25 11:40:24 +0100

    Pass test_twenty_four_with_day, test_two_days_later_with_day and test_high_duration_with_day.

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ from unittest import main
 # print('\nadd_time("11:59 PM", "24:05")\t|' + add_time("11:59 PM", "24:05") + '|\n')
 # print('\nadd_time("8:16 PM", "466:02")\t|' + add_time("8:16 PM", "466:02") + '|\n')
 # print('\nadd_time("5:01 AM", "0:00")\t|' + add_time("5:01 AM", "0:00") + '|\n')
-# print('\nadd_time("3:30 PM", "2:12", "Monday")\t|' + add_time("3:30 PM", "2:12", "Monday") + '|\n')
+# print('\nadd_time("3:30 PM", "2:12", "Monday")\t|' + add_time("3:30 PM", "2:12", "Monday") + '|\t5:42 PM, Monday\n')
+# print('\nadd_time("2:59 AM", "24:00", "saturDay")\t|' + add_time("2:59 AM", "24:00", "saturDay") + '|\t2:59 AM, Sunday (next day)\n')
+# print('\nadd_time("11:59 PM", "24:05", "Wednesday")\t|' + add_time("11:59 PM", "24:05", "Wednesday") + '|\t12:04 AM, Friday (2 days later)\n')
+# print('\nadd_time("8:16 PM", "466:02", "tuesday")\t|' + add_time("8:16 PM", "466:02", "tuesday") + '|\t6:18 AM, Monday (20 days later)\n')
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
--- time_calculator.py
+++ time_calculator.py
@@ -3,12 +3,14 @@ class time :
 
     periods = {0:'AM', 1:'PM'}
     periods_idx = {'AM':0, 'PM':1}
+    days = {1:'Monday', 2:'Tuesday', 3:'Wednesday', 4:'Thursday', 5:'Friday', 6:'Saturday', 7:'Sunday'}
+    days_idx = {'Monday':1, 'Tuesday':2, 'Wednesday':3, 'Thursday':4, 'Friday':5, 'Saturday':6, 'Sunday':7}
 
     hours = 0
     minutes = 0
     period = 0
-    day = ''
-    days_after = ''
+    day = None
+    days_after = None
 
     def __init__(self, time_as_string, day):
         tas = time_as_string.split()
@@ -17,7 +19,7 @@ class time :
         self.hours = int(t[0])
         self.minutes = int(t[1])
         if day :
-            self.day = ', ' + day
+            self.day = self.days_idx[day.title()]
 
     def add(self, duration):
         d = duration.split(':')
@@ -26,6 +28,10 @@ class time :
         h = divmod(self.hours + int(d[0]) + m[0], 12)
         self.hours = h[1]
         p = divmod(self.period + h[0], 2)
+        if self.day :
+            self.day += divmod(p[0], 7)[1]
+            if self.day > 7 :
+                self.day = self.day % 7
         if p[0] == 1 :
             self.days_after = '(next day)'
         if p[0] > 1 :
@@ -35,7 +41,12 @@ class time :
             self.hours = 12
 
     def __str__(self):
-        return f'{self.hours}:{self.minutes:02d} {self.periods[self.period]}{self.day} {self.days_after}'.rstrip()
+        str = f'{self.hours}:{self.minutes:02d} {self.periods[self.period]}'.rstrip()
+        if self.day :
+            str += f', {self.days[self.day]}'
+        if self.days_after :
+            str += f' {self.days_after}'
+        return str
 
 
 def add_time(start, duration, day = None):
