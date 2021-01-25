77401852d0819d70392a5021f53f796278f5c724
cheubes <christophe@heubes.com>
2021-01-25 09:03:00 +0100

    Pass test_same_period.

--- time_calculator.py
+++ time_calculator.py
@@ -1,6 +1,28 @@
+
+class time :
+    hours = 0
+    minutes = 0
+    period = 'AM'
+
+    def __init__(self, time_as_string):
+        tas = time_as_string.split()
+        self.period = tas[1]
+        t = tas[0].split(':')
+        self.hours = int(t[0])
+        self.minutes = int(t[1])
+
+    def add(self, duration):
+        d = duration.split(':')
+        self.hours += int(d[0])
+        self.minutes += int(d[1])
+
+    def __str__(self):
+        return f'{self.hours}:{self.minutes} {self.period}'
+
 def add_time(start, duration, day = None):
 
+    new_time = time(start)
+    new_time.add(duration)
+    print(f'{start} + {duration} = {new_time}')
 
-
-    new_time =''
-    return new_time
+    return str(new_time)
